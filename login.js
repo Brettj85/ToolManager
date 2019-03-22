d3266e0900bd1da1ebfa910c27e3ce40435c954f
Jackson <Brett.Jackson@charter.com>
2019-03-22 13:36:55 -0500

    seperated to manager page /user page

--- login.js
+++ login.js
@@ -1,1 +1,18 @@
-addEventListener
+var level = document.getElementById("UserLevel");
+
+var sub = document.querySelector(".SubmitButton")
+sub.addEventListener("click", function(e) {
+        OpenPage();
+      e.preventDefault();
+    });
+    function OpenPage()
+{
+    if (level.Value == "manager")
+    {
+        window.open("user.html");
+    }
+    else
+    {
+        window.open("user.html");
+    }
+}
